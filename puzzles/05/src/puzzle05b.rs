d5b4c9778f6650a1a336b19f8b749a31e3516569
Drew Deleón-Zemke <drew.zemke@gmail.com>
2025-12-04 22:21:53 -0800

    puzzle05: part b

--- puzzles/05/src/puzzle05b.rs
+++ puzzles/05/src/puzzle05b.rs
@@ -1,13 +1,22 @@
 use common::puzzle::PuzzlePart;
 
+use crate::IngredientDb;
+
 pub struct Puzzle05b {}
 
 impl PuzzlePart for Puzzle05b {
     fn description() -> &'static str {
-        "Puzzle 05 Part B"
+        "Count the total number of fresh ingredients in an ingredients."
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        let mut db = IngredientDb::parse(input);
+        db.merge_ranges();
+
+        db.ranges
+            .into_iter()
+            .map(|range| range.end() - range.start() + 1)
+            .sum::<u64>()
+            .to_string()
     }
 }
