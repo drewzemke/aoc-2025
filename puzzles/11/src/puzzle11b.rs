efb525ee2d3eb6a4a3226229e3e80d055d4203ca
Drew Deleón-Zemke <drew.zemke@gmail.com>
2025-12-14 16:12:13 -0800

    puzzle11: part b

--- puzzles/11/src/puzzle11b.rs
+++ puzzles/11/src/puzzle11b.rs
@@ -1,13 +1,37 @@
 use common::puzzle::PuzzlePart;
 
+use crate::Graph;
+
 pub struct Puzzle11b {}
 
 impl PuzzlePart for Puzzle11b {
     fn description() -> &'static str {
-        "Puzzle 11 Part B"
+        "Find the number of paths in a network that pass through two particular nodes"
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        let graph = Graph::parse(input);
+
+        let svr = graph.find_node("svr");
+        let fft = graph.find_node("fft");
+        let dac = graph.find_node("dac");
+        let out = graph.find_node("out");
+
+        // if a path passes through both fft and dac, then it either looks like
+        //   svr -- fft -- dac -- out
+        //  or
+        //   svr -- dac -- fft -- out
+        //
+        // ... can we just count segments and take products and out?
+        let svr_to_fft = graph.count_paths(svr, fft);
+        let fft_to_dac = graph.count_paths(fft, dac);
+        let dac_to_out = graph.count_paths(dac, out);
+
+        let svr_to_dac = graph.count_paths(svr, dac);
+        let dac_to_fft = graph.count_paths(dac, fft);
+        let fft_to_out = graph.count_paths(fft, out);
+
+        ((svr_to_fft * fft_to_dac * dac_to_out) + (svr_to_dac * dac_to_fft * fft_to_out))
+            .to_string()
     }
 }
